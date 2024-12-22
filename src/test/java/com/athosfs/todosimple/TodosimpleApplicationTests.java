904a1abd486905fd71e06c763385bdbfb380b665
Athoosz <usacrabrasilathos@gmail.com>
2024-12-22 18:46:16 -0300

    Spring Boot project

---
+++ src/test/java/com/athosfs/todosimple/TodosimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.athosfs.todosimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
