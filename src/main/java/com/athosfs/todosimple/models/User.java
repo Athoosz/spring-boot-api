84125e3989e8ff25f175977d79b76a1123e86002
Athoosz <usacrabrasilathos@gmail.com>
2024-12-22 23:34:56 -0300

    add task model

--- src/main/java/com/athosfs/todosimple/models/User.java
+++ src/main/java/com/athosfs/todosimple/models/User.java
@@ -1,23 +1,26 @@
 package com.athosfs.todosimple.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
 @Entity
 @Table(name = User.TABLE_NAME)
 public class User {
 
   public interface CreateUser {}
+
   public interface UpdateUser {}
 
   public static final String TABLE_NAME = "user";
@@ -37,16 +40,29 @@ public class User {
   @Column(name = "password", length = 60, nullable = false)
   @NotNull(groups = {CreateUser.class, UpdateUser.class})
   @NotEmpty(groups = {CreateUser.class, UpdateUser.class})
-  @Size(groups = {CreateUser.class, UpdateUser.class},min = 4, max = 60)
+  @Size(
+      groups = {CreateUser.class, UpdateUser.class},
+      min = 4,
+      max = 60)
   private String password;
 
-  public User() {
-  }
+  @OneToMany(mappedBy = "user") // um usuario tem varias tasks
+  private List<Task> tasks = new ArrayList<>();
+
+  public User() {}
 
   public User(Long id, String username, String password) {
     this.id = id;
     this.username = username;
     this.password = password;
+  }
+
+  public List<Task> getTasks() {
+    return tasks;
+  }
+
+  public void setTasks(List<Task> tasks) {
+    this.tasks = tasks;
   }
 
   public Long getId() {
@@ -85,31 +101,19 @@ public class User {
 
   @Override
   public boolean equals(Object obj) {
-    if (this == obj)
-      return true;
-    if (obj == null)
-      return false;
-    if (getClass() != obj.getClass())
-      return false;
+    if (this == obj) return true;
+    if (obj == null) return false;
+    if (getClass() != obj.getClass()) return false;
     User other = (User) obj;
     if (id == null) {
-      if (other.id != null)
-        return false;
-    } else if (!id.equals(other.id))
-      return false;
+      if (other.id != null) return false;
+    } else if (!id.equals(other.id)) return false;
     if (username == null) {
-      if (other.username != null)
-        return false;
-    } else if (!username.equals(other.username))
-      return false;
+      if (other.username != null) return false;
+    } else if (!username.equals(other.username)) return false;
     if (password == null) {
-      if (other.password != null)
-        return false;
-    } else if (!password.equals(other.password))
-      return false;
+      if (other.password != null) return false;
+    } else if (!password.equals(other.password)) return false;
     return true;
   }
-
-
-  
 }
