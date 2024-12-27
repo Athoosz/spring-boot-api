b711c90f9a3924999e617fa07934ad313abb453d
Athoosz <usacrabrasilathos@gmail.com>
2024-12-27 20:08:49 -0300

    atualizando front

--- view/js/script.js
+++ view/js/script.js
@@ -1,4 +1,4 @@
-const url = "http://localhost:8080/task/user/1";
+const tasksEndpoint = "http://localhost:8080/task/user";
 
 function hideLoader() {
    document.getElementById("loading").style.display = "none";
@@ -11,8 +11,6 @@ function show(tasks) {
                     <th scope="col">Title</th>
                     <th scope="col">Description</th>
                     <th scope="col">Username</th>
-                    <th scope="col">User Id</th>
-                    <th scope="col">Actions</th>
                 </tr>
             </thead>`;
     for (let task of tasks) {
@@ -20,8 +18,6 @@ function show(tasks) {
                     <th scope="row">${task.id}</th>
                     <td>${task.title}</td>
                     <td>${task.description}</td>
-                    <td>${task.user.username}</td>
-                    <td>${task.user.id}</td>
                     <td>
                         <button class="btn btn-success btn-sm" onclick="editTask(${task.id})">Edit</button>
                         <button class="btn btn-danger btn-sm" onclick="deleteTask(${task.id})">Delete</button>
@@ -32,15 +28,24 @@ function show(tasks) {
    document.getElementById("tasks").innerHTML = tab;
 }
 
-async function getApi(url) {
-   const response = await fetch(url, { method: "GET" });
-
-   var data = await response.json();
-   console.log(data);
-   if (response) {
-      hideLoader();
-      show(data);
-   }
-}
-
-getApi(url);
+async function getTasks() {
+    let key = "Authorization";
+    const response = await fetch(tasksEndpoint, {
+      method: "GET",
+      headers: new Headers({
+        Authorization: localStorage.getItem(key),
+      }),
+    });
+  
+    var data = await response.json();
+    console.log(data);
+    if (response) hideLoader();
+    show(data);
+  }
+  
+  document.addEventListener("DOMContentLoaded", function (event) {
+    if (!localStorage.getItem("Authorization"))
+      window.location = "/view/login.html";
+  });
+  
+  getTasks();
