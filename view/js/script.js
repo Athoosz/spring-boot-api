f358590bde47d8e29ba8167965297ccebc9deecc
Athoosz <usacrabrasilathos@gmail.com>
2024-12-28 14:05:33 -0300

    add function delete and post

--- view/js/script.js
+++ view/js/script.js
@@ -1,4 +1,5 @@
 const tasksEndpoint = "http://localhost:8080/task/user";
+const taskEndpoint = "http://localhost:8080/task";
 
 function hideLoader() {
    document.getElementById("loading").style.display = "none";
@@ -10,7 +11,7 @@ function show(tasks) {
                     <th scope="col">#</th>
                     <th scope="col">Title</th>
                     <th scope="col">Description</th>
-                    <th scope="col">Username</th>
+                    <th scope="col">Options</th>
                 </tr>
             </thead>`;
     for (let task of tasks) {
@@ -19,7 +20,6 @@ function show(tasks) {
                     <td>${task.title}</td>
                     <td>${task.description}</td>
                     <td>
-                        <button class="btn btn-success btn-sm" onclick="editTask(${task.id})">Edit</button>
                         <button class="btn btn-danger btn-sm" onclick="deleteTask(${task.id})">Delete</button>
                     </td>
                 </tr>`;
@@ -45,7 +45,51 @@ async function getTasks() {
   
   document.addEventListener("DOMContentLoaded", function (event) {
     if (!localStorage.getItem("Authorization"))
-      window.location = "/view/login.html";
+        window.location = "/view/login.html";
+
+    document.getElementById("task-form").addEventListener("submit", addTask);
+
+    getTasks();
+});
+
+async function addTask(event) {
+  event.preventDefault(); 
+
+  const title = document.getElementById("taskTitle").value;
+  const description = document.getElementById("taskDescription").value;
+
+  const response = await fetch(taskEndpoint, {
+      method: "POST",
+      headers: new Headers({
+          "Content-Type": "application/json",
+          Authorization: localStorage.getItem("Authorization"),
+      }),
+      body: JSON.stringify({
+          title: title,
+          description: description,
+      }),
   });
-  
-  getTasks();
+
+  if (response.ok) {
+      alert("Task adicionada com sucesso!");
+      location.reload(); 
+  } else {
+      alert("Falha ao adicionar task.");
+  }
+}
+
+async function deleteTask(taskId) {
+  const response = await fetch(`${taskEndpoint}/${taskId}`, {
+      method: "DELETE",
+      headers: new Headers({
+          Authorization: localStorage.getItem("Authorization"),
+      }),
+  });
+
+  if (response.ok) {
+      alert("Task deletada com sucesso!");
+      location.reload();
+  } else {
+      alert("Falha ao deletar task.");
+  }
+}
